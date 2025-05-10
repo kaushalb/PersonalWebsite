bb7c6c4edfa0df81325e30f5d1081f1dcae94e0f
KaushalB <kaushal.bommena@gmail.com>
2025-05-09 20:00:35 -0500

    resizes in font sizing

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -2,7 +2,7 @@ export default function AboutMe() {
     return (
       <section id="AboutMe" className="about--section">
         <div className="about--section--img">
-          <img src="./img/profile.jpeg" alt="About Me" />
+          <img src="./img/profile.jpeg" alt="About Me" className="circular-img"/>
         </div>
         <div className="hero--section--content--box about--section--box">
           <div className="hero--section--content">
